fbf9747af4905ce92cafe5419a90561294878ba1
Sayali <sayalibhosale909@gmail.com>
2019-04-30 11:31:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<h2><b>ABOUT US</b></h2>
+<p>Hello  Everyone!</p>
+
+<p>This "Online Notice Board" is brought to you by <b>Ms.Sayali Bhosale,Ms. Shraddha Dhasade, Mr.Pankaj Shinde </p>
+
+<br>
+
+<p>At "Online Notice Board" of College of Engineering Pandharpur, all the students, teaching and non-teaching staff
+are the core of our mission.</p>
+ <br>
+ <p>We’re obsessed about the up-to-date all the members - above all - our 
+ team. Everyday, we strive to empower and support our teammates to help them realize their full 
+ potential.</p>
